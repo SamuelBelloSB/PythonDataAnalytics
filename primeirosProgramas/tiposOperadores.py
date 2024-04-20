5fdb2257d6441a0ae8ed3d87ede811d3304352ae
Smauel Bello <samuelbellocontato@gmail.com>
2024-04-20 16:00:36 -0300

    Tipos de operadores python

---
+++ primeirosProgramas/tiposOperadores.py
@@ -0,0 +1,22 @@
+valorA, valorB = 10, 5
+
+#   Adição
+print(f"Adicao {valorA} + {valorB} = {valorA+valorB}")
+
+#   Subtracao
+print(f"Subtracao {valorA} - {valorB} = {valorA-valorB}")
+
+#   Multiplicação
+print(f"Multiplicacao {valorA} * {valorB} = {valorA*valorB}")
+
+#   Divisão
+print(f"Divisao {valorA} / {valorB} = {valorA/valorB}")
+
+#   Divisao inteira
+print(f"Divisao inteira {valorA} // {valorB} = {valorA//valorB}")
+
+#   Resto da divisao/Módulo
+print(f"Modulo/Resto da divisao {valorA} % {valorB} = {valorA%valorB}")
+
+#   Exponenciação
+print(f"Exponenciacao {valorA} ** {valorB} = {valorA**valorB}")
