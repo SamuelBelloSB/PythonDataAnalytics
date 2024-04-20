b233f4e6ec6077803fd1b4996af7f4bc6f30889b
Smauel Bello <samuelbellocontato@gmail.com>
2024-04-20 16:09:34 -0300

    Operadores de comparação em python

---
+++ primeirosProgramas/tiposOperadoresComparacao.py
@@ -0,0 +1,7 @@
+saldo = 452
+saque = 168
+print(saldo<saque)  #   Saldo menor que saque?
+print(saldo>saque)  #   Saldo maior que saque?
+print(saldo<=saque) #   Saldo é menor ou igual a saque?
+print(saldo>=saque) #   Saldo é maior ou igual a saque?
+print(saldo!=saque) #   Saldo é diferente de sague?
