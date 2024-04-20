dd33ab07b964a5be19e0206ca280e84b8f8116d0
Smauel Bello <samuelbellocontato@gmail.com>
2024-04-20 15:51:06 -0300

    introdução

---
+++ primeirosProgramas/tiposDados.py
@@ -0,0 +1,9 @@
+'''
+Text        -> Str
+Númerico    -> int, float, complex
+Sequência   -> list, tuple, range
+Mapa        -> dict
+Coleção     -> set, frozenset
+Booleano    -> bool
+Binário     -> bytes, bytearray, memoryview
+'''
